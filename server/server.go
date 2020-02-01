6ccdb9b878c6ae38198dc6dd00b4702ccb21f16d
Alexander Zaytsev <me@axv.email>
2020-02-01 17:53:22 +0300

    echo server

--- server/server.go
+++ server/server.go
@@ -2,19 +2,29 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Package main is a simple echo server.
 package main
 
 import (
 	"flag"
 	"fmt"
 	"io"
+	"log"
 	"net"
+	"os"
+)
+
+var (
+	// internal logger
+	info = log.New(os.Stdout, fmt.Sprintf("INFO [server]: "), log.Ldate|log.Ltime)
 )
 
 func handleConnection(c net.Conn) {
 	defer func() {
-		fmt.Printf("\nclose from %v\n", c.RemoteAddr())
-		c.Close()
+		info.Printf("close connection from %v\n", c.RemoteAddr())
+		if err := c.Close(); err != nil {
+			info.Printf("failed conneciton close: %v\n", err)
+		}
 	}()
 	b := make([]byte, 8)
 	for {
@@ -23,13 +33,13 @@ func handleConnection(c net.Conn) {
 		case err == io.EOF:
 			return
 		case err != nil:
-			fmt.Printf("error: %v\n", err)
+			info.Printf("error: %v\n", err)
 		default:
 			// 'q'
 			if (n > 0) && b[0] == 113 {
 				return
 			}
-			fmt.Printf("%v", b[:n])
+			info.Printf("%v", b[:n])
 			_, err = c.Write(b[:n])
 			if err != nil {
 				return
@@ -41,18 +51,19 @@ func handleConnection(c net.Conn) {
 func main() {
 	port := flag.Uint("port", 10000, "listen port")
 	flag.Parse()
+
 	conStr := fmt.Sprintf(":%v", *port)
 	ln, err := net.Listen("tcp", conStr)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("listen: %v\n", conStr)
+	info.Printf("listen: %v\n", conStr)
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
 			panic(err)
 		}
-		fmt.Printf("\naccept from %v\n", conn.RemoteAddr())
+		info.Printf("accept connection from %v\n", conn.RemoteAddr())
 		go handleConnection(conn)
 	}
 }
