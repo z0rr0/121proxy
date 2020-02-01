b05a3b7c969b028732774b68b282892df996391b
Alexander Zaytsev <me@axv.email>
2020-02-01 17:35:13 +0300

    copyright update

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2015, Alexander Zaytsev. All rights reserved.
+// Copyright (c) 2020, Alexander Zaytsev <me@axv.email>. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
